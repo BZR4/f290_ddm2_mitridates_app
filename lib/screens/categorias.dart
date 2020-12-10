816cf22eb3ecb5bc5f4a4c2b20616a3f3ddf95ed
Esdras B Silva <esdrasilva@gmail.com>
2020-12-10 20:24:01 -0300

    Inclusao de SVG e inicio de criacao de cards na tela de categorias.

--- lib/screens/categorias.dart
+++ lib/screens/categorias.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class CategoriaPage extends StatefulWidget {
   @override
@@ -8,8 +9,60 @@ class CategoriaPage extends StatefulWidget {
 class _CategoriaPageState extends State<CategoriaPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.deepPurple),
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Selecione um dos cards para filtrar os alunos',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ),
+          Card(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  SvgPicture.asset(
+                    "images/estagio.svg",
+                    height: 150,
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Estágio',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    subtitle:
+                        Text('Conheça nossos alunos disponíneis para estágio.'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Card(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  SvgPicture.asset(
+                    "images/trabalho.svg",
+                    height: 150,
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Trabalho',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    subtitle: Text(
+                        'Conheça nossos alunos com disponibilidade para trabalho em tempo integral.'),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
